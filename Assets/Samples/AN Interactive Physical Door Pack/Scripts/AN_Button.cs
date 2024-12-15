23fc40b221381de92d427ccc295ce8712179ef6d
Anoth <mixaetel@gmail.com>
2024-12-15 22:24:20 +0300

    init

---
+++ Assets/Samples/AN Interactive Physical Door Pack/Scripts/AN_Button.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AN_Button : MonoBehaviour
+{
+    [Tooltip("If it isn't valve, it can be lever or button (animated)")]
+    public bool isLever = true;
+    [Tooltip("If it is false door can't be used")]
+    public bool Locked = false;
+    [Tooltip("The door for remote control")]
+    public AN_DoorScript DoorObject;
+    [Space]
+    [Tooltip("Any object for ramp/elevator baheviour")]
+    public Transform RampObject;
+    [Tooltip("Door can be opened")]
+    public bool CanOpen = true;
+    [Tooltip("Door can be closed")]
+    public bool CanClose = true;
+    [Tooltip("Current status of the door")]
+    public bool isOpened = false;
+    [Space]
+    [Tooltip("True for rotation by X local rotation by valve")]
+    public bool xRotation = true;
+    [Tooltip("True for vertical movenment by valve (if xRotation is false)")]
+    public bool yPosition = false;
+    public float max = 90f, min = 0f, speed = 5f;
+
+    public InputActionReference interactAction;
+
+    Animator anim;
+
+    // NearView()
+    float distance;
+    float angleView;
+    Vector3 direction;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!Locked)
+        {
+            if (interactAction.action.IsPressed() && DoorObject != null && DoorObject.Remote && NearView()) // 1.lever and 2.button
+            {
+                DoorObject.Action(); // void in door script to open/close
+                if (isLever) // animations
+                {
+                    if (DoorObject.isOpened) anim.SetBool("LeverUp", true);
+                    else anim.SetBool("LeverUp", false);
+                }
+                else anim.SetTrigger("ButtonPress");
+            }
+        }
+    }
+
+    bool NearView() // it is true if you near interactive object
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        direction = transform.position - Camera.main.transform.position;
+        angleView = Vector3.Angle(Camera.main.transform.forward, direction);
+        if (angleView < 45f && distance < 2f) return true;
+        else return false;
+    }
+}
